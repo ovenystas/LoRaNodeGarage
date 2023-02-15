2079277f923f59ba1493d7c321d5214490e42045
Ove Nystås <ove.nystas@gmail.com>
2023-02-15 22:32:09 +0100

    Add pre-commit hooks cmake-format and cmake-lint

--- cmake/CodeCoverage.cmake
+++ cmake/CodeCoverage.cmake
@@ -1,28 +1,53 @@
-OPTION( ENABLE_CODECOVERAGE "Enable code coverage testing support" )
+option(ENABLE_CODECOVERAGE "Enable code coverage testing support")
 
-if ( ENABLE_CODECOVERAGE )
+if(ENABLE_CODECOVERAGE)
 
-    if ( NOT CMAKE_BUILD_TYPE STREQUAL "Debug" )
-        message( WARNING "Code coverage results with an optimised (non-Debug) build may be misleading" )
-    endif ()
+  if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
+    message(
+      WARNING
+        "Code coverage results with an optimised (non-Debug) build may be misleading"
+    )
+  endif()
 
-    if ( NOT DEFINED CODECOV_OUTPUTFILE )
-        set( CODECOV_OUTPUTFILE cmake_coverage.output )
-    endif ()
+  if(NOT DEFINED CODECOV_OUTPUTFILE)
+    set(CODECOV_OUTPUTFILE cmake_coverage.output)
+  endif()
 
-    if ( NOT DEFINED CODECOV_HTMLOUTPUTDIR )
-        set( CODECOV_HTMLOUTPUTDIR coverage_results )
-    endif ()
+  if(NOT DEFINED CODECOV_HTMLOUTPUTDIR)
+    set(CODECOV_HTMLOUTPUTDIR coverage_results)
+  endif()
 
-    if ( CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUCXX )
-        find_program( CODECOV_GCOV gcov )
-        find_program( CODECOV_LCOV lcov )
-        find_program( CODECOV_GENHTML genhtml )
-        add_definitions( -fprofile-arcs -ftest-coverage )
-        link_libraries( gcov )
-        set( CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} --coverage )
-        add_custom_target( coverage_init ALL ${CODECOV_LCOV} --base-directory . --directory ${CMAKE_BINARY_DIR} --output-file ${CODECOV_OUTPUTFILE} --capture --initial )
-        add_custom_target( coverage ${CODECOV_LCOV} --base-directory . --directory ${CMAKE_BINARY_DIR} --output-file ${CODECOV_OUTPUTFILE} --capture COMMAND genhtml -o ${CODECOV_HTMLOUTPUTDIR} ${CODECOV_OUTPUTFILE} )
-    endif ()
+  if(CMAKE_COMPILER_IS_GNUCXX OR CMAKE_COMPILER_IS_GNUCXX)
+    find_program(CODECOV_GCOV gcov)
+    find_program(CODECOV_LCOV lcov)
+    find_program(CODECOV_GENHTML genhtml)
+    add_definitions(-fprofile-arcs -ftest-coverage)
+    link_libraries(gcov)
+    set(CMAKE_EXE_LINKER_FLAGS ${CMAKE_EXE_LINKER_FLAGS} --coverage)
+    add_custom_target(
+      coverage_init ALL
+      ${CODECOV_LCOV}
+      --base-directory
+      .
+      --directory
+      ${CMAKE_BINARY_DIR}
+      --output-file
+      ${CODECOV_OUTPUTFILE}
+      --capture
+      --initial
+      COMMENT "Coverage init")
+    add_custom_target(
+      coverage
+      ${CODECOV_LCOV}
+      --base-directory
+      .
+      --directory
+      ${CMAKE_BINARY_DIR}
+      --output-file
+      ${CODECOV_OUTPUTFILE}
+      --capture
+      COMMAND genhtml -o ${CODECOV_HTMLOUTPUTDIR} ${CODECOV_OUTPUTFILE}
+      COMMENT "Coverage")
+  endif()
 
-endif ()
+endif()
